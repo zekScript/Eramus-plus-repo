27948cd6e708c3ec01e6c3614d6d6a1029a7037a
zekScript <armandas.latanauskas06@gmail.com>
2024-11-05 12:19:03 +0100

    Made a authentication to register with google but there's an error

--- src/components/layout/navs/main-nav.tsx
+++ src/components/layout/navs/main-nav.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import * as React from 'react'
-
 import { NavItem } from '@/types'
-
-
-
+import SignIn from './sign-in'
 import Link from 'next/link'
+import { topNav } from '@/config/site'
+import { title } from 'process'
+
 
 interface MainNavProps {
   items?: NavItem[]
@@ -17,20 +17,27 @@ const MainNav: React.FC<MainNavProps> = ({ items }) => {
   // const segment = useSelectedLayoutSegment()
   // console.log('🚀 ~ segment:', segment)
 
-
-  
+  const topNavItems = topNav.items[0].title;
 
   return (
     <>
-    <div className='hidden lg:flex'>
+    {/* This SHOULD BE HIDDEN */}
+    <div className=' lg:flex'>
       {/* MENU */}
-      <div>
-        <Link href='/'>Home</Link>
+      <div className='flex gap-12 mr-12' >
+        <Link href='#'>{topNavItems}</Link>
         <Link href='/create'>Create</Link>
-        <Link href='/create-account'>Register</Link>
-        <Link href='/log-account'>Log in</Link>
+  
       </div>
+      
+      
     </div>
+  {/* <div className=' w-full'>
+      <div className="w-auto flex gap-6 text-end ">
+      <Link href='/log-account'>Log in</Link>
+      <SignIn></SignIn>
+      </div>
+  </div> */}
     </>
   )
 }
