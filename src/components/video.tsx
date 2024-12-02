dc7dc60a7cfb31262797ac396c22d536d7fea5f6
zekScript <armandas.latanauskas06@gmail.com>
2024-12-02 11:12:35 +0100

    Small change npm start is good

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -5,15 +5,14 @@ import { carouselItems } from '@/config/site'
 
 const Video: React.FC = () => {
   return (
-    <div className='min-h-[100vh] w-full overflow-hidden bg-black relative'> {/* Apply relative positioning */}
+    <div className='relative min-h-[100vh] w-full overflow-hidden bg-black'>
+      {' '}
+      {/* Apply relative positioning */}
       <div className='absolute z-30 flex h-full w-full items-center justify-center overflow-hidden'>
-        <FadeInContent
-          carouselItems={carouselItems.carouselItems}
-        />
+        <FadeInContent carouselItems={carouselItems.carouselItems} />
       </div>
-
       {/* Background video with pointer-events: none to avoid blocking interactions */}
-      <div className='absolute z-[1] h-full w-full object-cover opacity-50'>
+      <div className='absolute z-[1] h-full w-full object-cover opacity-30'>
         <video
           className='absolute z-[-1] h-full w-full object-cover'
           playsInline
