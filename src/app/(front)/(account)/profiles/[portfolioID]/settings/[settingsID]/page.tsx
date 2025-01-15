59e1d14fd424a4b110e005aa81d1233ab46fca12
zekScript <armandas.latanauskas06@gmail.com>
2025-01-15 20:06:15 +0200

    Made some new comments and css

--- src/app/(front)/(account)/profiles/[portfolioID]/settings/[settingsID]/page.tsx
+++ src/app/(front)/(account)/profiles/[portfolioID]/settings/[settingsID]/page.tsx
@@ -11,7 +11,7 @@ export default function SettingsPage() {
   const user = getCurrentUser()
 
   useEffect(() => {
-    // Redirect to /general if the current path doesn't include '/general'
+    // Redirect to /general if the current path doesn't include these routes below
     if (
       !(
         pathname.includes('/general') ||
