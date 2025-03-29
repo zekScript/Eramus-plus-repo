ca8b2f9f9d8bffd4776a491151874f5480b1772c
zekScript <armandas.latanauskas06@gmail.com>
2025-03-29 09:04:28 +0200

    transfer to main pc

--- src/app/(front)/(account)/profiles/[portfolioID]/create/page.tsx
+++ src/app/(front)/(account)/profiles/[portfolioID]/create/page.tsx
@@ -42,40 +42,85 @@ export default function CreatePost() {
   }
 
   return (
-    <div className='m-auto w-[90%] max-w-2xl p-6'>
-      <h1 className='text-2xl font-bold'>Create a New Blog Post</h1>
-      {error && <p className='text-red-500'>{error}</p>}
-      <form action={handleSubmit} className='mt-4'>
-        <input type='hidden' name='userID' defaultValue={userId}></input>
-        <input
-          type='text'
-          name='title'
-          placeholder='Post Title'
-          className='mb-4 w-full rounded border p-2'
-          required
-        />
-        <textarea
-          name='content'
-          placeholder='Write your post in Markdown...'
-          className='h-40 w-full rounded border p-2'
-          required
-        />
-        <button
-          type='submit'
-          className='mt-4 w-full rounded bg-blue-600 p-2 text-white'
-        >
-          Send data
-        </button>
-        {feedback.message && (
-          <div
-            className={`ml-5 mt-4 w-full p-3 text-start ${
-              feedback.success ? 'text-green-700' : 'text-red-700'
-            }`}
-          >
-            {feedback.message}
-          </div>
-        )}
-      </form>
+    <div className='bg-red-500 w-full h-full'>
+        {/* TODO: Add a sidebar where u can esilly create or edit blog posts and save it as a snipet for later projects */}
+        {/* TODO: add a window where a user can enter text in markdown and color it in markdown */}
+        {/* TODO: and in real time always check how is the output */}
+
+{/* Sidebar */}
+<div>
+<p>Hello world</p>
+    <p>Hello world</p>
+    <p>Hello world</p>
+    <p>Hello world</p>
+    <p>Hello world</p>
+    <p>Hello world</p>
+</div>
+
+{/* Input window */}
+<div>
+  <div>
+    {/* Juosta */}
+
+  </div>
+  <div>
+    {/* Title input */}
+  </div>
+  <div>
+    {/* Writing window */}
+  </div>
+</div>
+    
+
+    {/* Output window */}
+
+    <div>
+      
+    </div>
+
     </div>
   )
 }
+
+
+
+
+
+
+
+
+// <div className='m-auto w-[90%] max-w-2xl p-6'>
+//       <h1 className='text-2xl font-bold'>Create a New Blog Post</h1>
+//       {error && <p className='text-red-500'>{error}</p>}
+//       <form action={handleSubmit} className='mt-4'>
+//         <input type='hidden' disabled name='userID' defaultValue={userId}></input>
+//         <input
+//           type='text'
+//           name='title'
+//           placeholder='Post Title'
+//           className='mb-4 w-full rounded border p-2'
+//           required
+//         />
+//         <textarea
+//           name='content'
+//           placeholder='Write your post in Markdown...'
+//           className='h-40 w-full rounded border p-2'
+//           required
+//         />
+//         <button
+//           type='submit'
+//           className='mt-4 w-full rounded bg-blue-600 p-2 text-white'
+//         >
+//           Send data
+//         </button>
+//         {feedback.message && (
+//           <div
+//             className={`ml-5 mt-4 w-full p-3 text-start ${
+//               feedback.success ? 'text-green-700' : 'text-red-700'
+//             }`}
+//           >
+//             {feedback.message}
+//           </div>
+//         )}
+//       </form>
+//     </div>
